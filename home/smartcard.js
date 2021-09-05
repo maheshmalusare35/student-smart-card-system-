e4b8f58bd84b722faba89e61bfb3c52b381c6f99
Mahesh <maheshmalusare35@gmail.com>
2021-09-05 14:15:06 +0530

    commit

--- home/smartcard.js
+++ home/smartcard.js
@@ -10,16 +10,16 @@ $(function(){
 
   /*login form hide and show */
 
-  $("#signup").click(function () {
-    $("#first").fadeOut("fast", function () {
-      $("#second").fadeIn("fast");
-    });
-  });
+  // $("#signup").click(function () {
+  //   $("#first").fadeOut("fast", function () {
+  //     $("#second").fadeIn("fast");
+  //   });
+  // });
 
-  $("#signin").click(function () {
-    $("#second").fadeOut("fast", function () {
-      $("#first").fadeIn("fast");
-    });
-  });
+  // $("#signin").click(function () {
+  //   $("#second").fadeOut("fast", function () {
+  //     $("#first").fadeIn("fast");
+  //   });
+  // });
   
   
