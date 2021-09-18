dee1e4dcfe7945d7afaf88dfcfc8252ebff320b7
Mahesh <maheshmalusare35@gmail.com>
2021-09-18 12:45:02 +0530

    commit

--- smart card system/smartcard.js
+++ smart card system/smartcard.js
@@ -22,4 +22,37 @@ $(function(){
   //   });
   // });
   
-  
+/* scroll effect*/
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        // if(this.scrollY > 20){
+        //     $('.navbar').addClass("sticky");
+        // }else{
+        //     $('.navbar').removeClass("sticky");
+        // }
+
+        if(this.scrollY > 100){            
+            $('.navbar').css('background', 'linear-gradient(45deg, #ffaf00, #bb02ff)');           
+        }else{           
+            $('.navbar').css('background', '');           
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");           
+        }else{
+            $('.scroll-up-btn').removeClass("show");          
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    
+   
+});
