711ae4c08a48046121eafc26f245958791028ab5
Mahesh <maheshmalusare35@gmail.com>
2021-09-22 13:11:13 +0530

    commit

--- smart card system/smartcard.js
+++ smart card system/smartcard.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
+        $('html').css("scrollBehavior", "smooth");
     });
 
     
