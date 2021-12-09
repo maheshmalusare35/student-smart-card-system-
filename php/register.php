1db29b4569fde0e1701cdbb83fbe7cf5439fc795
Mahesh <maheshmalusare35@gmail.com>
2021-12-09 15:24:31 +0530

    commit

---
+++ php/register.php
@@ -0,0 +1,86 @@
+<?php
+        include '../database/databaseconnection.php';
+
+        if(isset($_POST['submit']))
+        {
+         $firstname=$_POST['firstname'];
+         $middlename=$_POST['middlename'];
+         $lastname=$_POST['lastname'];
+         $email=$_POST['email'];
+         $phone=$_POST['phone'];
+         $role=$_POST['role'];
+         $gender=$_POST['gender'];
+         $date=$_POST['date'];
+         $pass=$_POST['pass'];
+         $year=$_POST['year'];
+
+
+
+         $query= mysqli_query($conn,"SELECT * FROM register WHERE email='$email'");
+        if (mysqli_num_rows($query)>0) 
+        {
+                       ?>
+                       <script >
+                       alert("Email is already use....");
+                       </script>
+                       <?php
+                      ?>
+                      <script >
+                        location.replace("../register.html");
+                       </script>
+                       <?php
+        }
+        else
+        {
+
+        $query= mysqli_query($conn,"SELECT * FROM register WHERE phoneno='$phoneno'");
+        if (mysqli_num_rows($query)>0) 
+        {
+                       ?>
+                       <script >
+                       alert("Phoneno is already use....");
+                       </script>
+                       <?php
+                        ?>
+                        <script >
+                          location.replace("../register.html");
+                         </script>
+                         <?php
+        }
+        else{
+
+
+
+            $sql = mysqli_query($conn,"INSERT INTO register VALUES('$firstname','$middlename','$lastname','$email','$phone','$role','$gender','$date','$pass','$year')");
+
+            if($sql)
+            {
+                ?>
+                       <script >
+                       alert("Record insert successfully");
+                       </script>
+                       <?php
+                       ?>
+                       <script >
+                         location.replace("../register.html");
+                        </script>
+                        <?php
+            }
+            else
+            {
+                ?>
+                       <script >
+                       alert("Record insert failed");
+                       </script>
+                       <?php
+                        ?>
+                        <script >
+                          location.replace("../register.html");
+                         </script>
+                         <?php
+                       
+            }
+        }
+    }
+        }
+?>
