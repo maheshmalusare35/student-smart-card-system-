0b45aa158ec629961ac018a861323917b2bb704c
Mahesh <maheshmalusare35@gmail.com>
2022-01-03 11:53:24 +0530

    commit

--- mailfunction/emailverify.php
+++ mailfunction/emailverify.php
@@ -1,15 +1,16 @@
 <?php
+session_start();
 include "mail_function.php";
-include "../database/databaseconnection.php";
 date_default_timezone_set("Asia/Kolkata");
 $success = "";
+$error_message = "";
+include '../database/databaseconnection.php';
 
-
-if(!empty($_POST["submit_email"])) {
-    $email=$_POST["email"];
-    $_SESSION['email']=$_POST["email"];
-    $result = mysqli_query($conn,"SELECT * FROM register WHERE email='" . $_POST["email"] . "'");
-    /*$count  = mysqli_num_rows($result);*/
+if(isset($_POST["submit_email"])) {
+    $email=$_POST['email'];
+   
+    $result = mysqli_query($conn,"SELECT * FROM register WHERE email='" . $email . "'");
+    /*$count  = mysqli_num_rows($result);*/  
     if(mysqli_num_rows($result)>0) {
         // generate OTP
         $otp = rand(100000,999999);
@@ -19,30 +20,57 @@ if(!empty($_POST["submit_email"])) {
         $mail_status = sendOTP($_POST["email"],$otp);
          $mail_status=1; 
         if($mail_status == 1) {
-            $result = mysqli_query($conn,"UPDATE register SET otp='".$otp."',is_expired='0', create_at='" .date("Y-m-d H:i:s"). "' WHERE email='".$email."' ");
+            $result = mysqli_query($conn,"UPDATE register SET otp = '" . $otp . "', is_expired = '0', create_at = '" . date("Y-m-d H:i:s") . "' WHERE email = '" .  $email . "' "); 
             // $current_id = mysqli_insert_id($conn);
-        
+      $_SESSION['EMAIL'] = $email;
                 $success=1;
+                
             
         }
     } else {
-      
+        // $error_message = "Email not exists!";
         echo "<script>alert('oops! Email not exists!')</script>";
     }
 }
-if(!empty($_POST["submit_otp"])) {
+if(isset($_POST["submit_otp"])) {
     $result = mysqli_query($conn,"SELECT * FROM register WHERE otp='" . $_POST["otp"] . "' AND is_expired!=1 AND NOW() <= DATE_ADD(create_at, INTERVAL 1 MINUTE)");
     // $count  = mysqli_num_rows($result);
     if(!empty(mysqli_num_rows($result))) {
         $result = mysqli_query($conn,"UPDATE register SET is_expired = 1 WHERE otp = '" . $_POST["otp"] . "'");
-        $success = 2;   
+               $success = 2;   
     } else {
         $success = 1;
-       
+        // $error_message = "Invalid OTP!";
          echo "<script>alert('oops! Invalid OTP!')</script>";
 
     }   
 }
+
+
+ if(isset($_POST["send_otp"])) {
+  $email = $_SESSION['EMAIL'];
+      
+     $result = mysqli_query($conn,"SELECT * FROM register WHERE email='" . $email . "'");
+    /*$count  = mysqli_num_rows($result);*/  
+    if(mysqli_num_rows($result)>0) {
+        // generate OTP
+        $otp = rand(100000,999999);
+        
+        // Send OTP
+        require_once("mail_function.php");
+        $mail_status = sendOTP($email,$otp);
+         $mail_status=1; 
+        if($mail_status == 1) {
+            $result = mysqli_query($conn,"UPDATE register SET otp = '" . $otp . "', is_expired = '0', create_at = '" . date("Y-m-d H:i:s") . "' WHERE email = '" .  $email . "' "); 
+            // $current_id = mysqli_insert_id($conn);
+     
+               
+                 $success=1;
+            
+        }
+    } 
+}
+
 ?>
 
 
@@ -103,6 +131,19 @@ if(!empty($_POST["submit_otp"])) {
             margin-right: 100px;
         } */
 
+
+         .blink_me
+           {
+                animation: blinker 1s linear infinite;
+            }
+
+            @keyframes blinker
+             {
+                    50% {
+                        opacity: 0;
+                    }
+             }
+
       
     </style>
 
@@ -114,7 +155,8 @@ if(!empty($_POST["submit_otp"])) {
 
 
     <div class="container-fluid">
-     
+
+       
  
         <div class=" align-items-center">
 
@@ -135,19 +177,21 @@ if(!empty($_POST["submit_otp"])) {
                     { 
         		?>
             
-                     <h1 class="text-center fw-bold mb-2"><span class="multicolortext">Email Verification</span> </h1>
+            <h1 class="text-center fw-bold mb-2"><span class="multicolortext">Email Verification</span> </h1>
                      <hr>
                       <div class="form-group mb-3">
                         <label for="otp" class="mb-3">Check your Email to get otp:</label>
-                        <input type="text" name="otp" id="otp" class="form-control" placeholder="Type otp" maxlength="6"
-                            autocomplete="off">
-                      </div>                
+                        <input type="text" name="otp" id="otp" class="form-control" placeholder="Type otp" 
+                            autocomplete="off" maxlength="6">
+                            <h6 id="otpcheck"></h6>
+                      </div>
+                      <div class="blink_me mb-3 text-center" style="color: red;"> <h5> OTP expires in <span id="timer"></span> </h5></div>                
                                       
                        <div class=" gap-2 mb-3 row align-items-center justify-content-center">
                         <button type="submit" name="submit_otp" value="submit" class="btn btn-primary btn-lg col-md-4">Verify OTP</button>                  
-                        <a href="#" class="btn btn-primary btn-lg col-md-4" type="submit" name="send_otp" value="send_otp">Send OTP</a>
+                        <button class="btn btn-primary btn-lg col-md-4" type="submit" name="send_otp" value="submit">Resend OTP</button>
                        </div>
-                    
+
 
                <?php 
 			      }
@@ -157,7 +201,7 @@ if(!empty($_POST["submit_otp"])) {
                    {
 				?>
                    <script >
-                    location.replace("../index.html");
+                    location.replace("../login.html");
                    </script>
                  <?php
 			    
@@ -176,6 +220,7 @@ if(!empty($_POST["submit_otp"])) {
                         <label for="email" class="mb-3">Enter your Email id use in registration:</label>
                         <input type="text" name="email" id="email" class="form-control" placeholder="name@example.com"
                             autocomplete="off">
+                            <h6 id="emailcheck"></h6>
                     </div>                
                                       
                     <div class="d-grid gap-2 col-6 mx-auto mb-3">
@@ -196,6 +241,176 @@ if(!empty($_POST["submit_otp"])) {
     <!-- Bootstrap JS -->
     <script type="text/javascript" src="../js/bootstrap.bundle.min.js"> </script>
 
+
+    <!-- javascript -->
+    <script type="text/javascript">
+
+       
+
+        // for the check the form
+
+        $(document).ready(function () {
+
+
+            // for the email jquery
+
+        $("#emailcheck").hide();
+
+        $("#otpbtn").hide();
+        $("#otpvalid").hide();
+
+        var email_err = true;
+
+        $("#email").keyup(function () {
+          email_check();
+        });
+
+        function email_check() {
+          var email_val = $("#email").val();
+
+          if (email_val.length == "") {
+            $("#emailcheck").show();
+            $("#emailcheck").html("*please fill the email address*");
+            $("#emailcheck").focus();
+            $("#emailcheck").css("color", "red");
+            email_err = false;
+            return false;
+          } else {
+            $("#emailcheck").hide();
+          }
+
+          if (
+            !email_val.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)
+          ) {
+            $("#emailcheck").show();
+            $("#emailcheck").html("*please fill the valid email address*");
+            $("#emailcheck").focus();
+            $("#emailcheck").css("color", "red");
+            email_err = false;
+            return false;
+          } else {
+            $("#emailcheck").hide();
+          }
+        }
+
+
+           
+            // for the otp jquery
+
+        $("#otpcheck").hide();
+
+        var otp_err = true;
+
+        $("#otp").keyup(function () {
+          otp_check();
+        });
+
+        function otp_check() {
+          var otp_val = $("#otp").val();
+
+          if (otp_val.length == "") {
+            $("#otpcheck").show();
+            $("#otpcheck").html("*please fill the OTP no*");
+            $("#otpcheck").focus();
+            $("#otpcheck").css("color", "red");
+            otp_err = false;
+            return false;
+          } else {
+            $("#otpcheck").hide();
+          }
+       
+
+          if (otp_val.match(/([a-zA-Z])/)) {
+            $("#otpcheck").show();
+            $("#otpcheck").html("*please fill the valid OTP no*");
+            $("#otpcheck").focus();
+            $("#otpcheck").css("color", "red");
+            otp_err = false;
+            return false;
+          } else {
+            $("#otpcheck").hide();
+          }
+        }
+            
+
+
+
+          
+            
+            // for the check submit email
+            $('#submit_email').click(function () {
+
+                           
+                            email_err = true;           
+                           
+                           email_check();                
+
+                            
+                         if  (email_err == true) {
+
+                                return true;
+                            }
+                            else {
+                                return false;
+                            }
+                        });
+
+
+
+            // for the check submit otp
+                $('#submit_otp').click(function () {
+
+                               
+                                otp_err = true;                             
+
+                               otp_check();
+                               
+                                
+                             if (otp_err == true)  {
+
+                                    return true;
+                                }
+                                else {
+                                    return false;
+                                }
+                            });
+
+
+           
+        });
+    </script>
+
+<script type="text/javascript">
+        //for otp time expiry
+        let timerOn = true;
+
+function timer(remaining) {
+  var m = Math.floor(remaining / 60);
+  var s = remaining % 60;
+  
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+  document.getElementById('timer').innerHTML = m + ':' + s;
+  remaining -= 1;
+  
+  if(remaining >= 0 && timerOn) {
+    setTimeout(function() {
+        timer(remaining);
+    }, 1000);
+    return;
+  }
+
+  if(!timerOn) {
+    // Do validate stuff here
+    return;
+  }
+  
+  // Do timeout stuff here
+  alert(' Oops! OTP was expire please click on Resend otp');
+}
+
+timer(60);
+    </script>
 
 
 </body>
