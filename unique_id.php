f192b252d6ff026b243c38c318b7007a3e9cc7ec
Mahesh <maheshmalusare35@gmail.com>
2022-01-22 11:54:31 +0530

    commit

---
+++ unique_id.php
@@ -0,0 +1,19 @@
+<?php 
+$randomid = mt_rand(100000,250000); 
+echo"100$randomid"; 
+?>
+<br>
+<?php 
+$randomid = mt_rand(260000,500000); 
+echo"200$randomid"; 
+?>
+<br>
+<?php 
+$randomid = mt_rand(510000,750000); 
+echo"300$randomid"; 
+?>
+<br>
+<?php 
+$randomid = mt_rand(760000,999999); 
+echo"400$randomid"; 
+?>
