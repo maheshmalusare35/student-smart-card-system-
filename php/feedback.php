3b797422a226bc5ae963d35c00391059080a09e5
Mahesh <maheshmalusare35@gmail.com>
2022-03-22 21:13:50 +0530

    commit

---
+++ php/feedback.php
@@ -0,0 +1,46 @@
+<?php
+
+
+        include '../database/databaseconnection.php';
+
+        if(isset($_POST['submit']))
+        {
+            $name = $_POST['name'];            
+            $email = $_POST['email'];
+            $comments = $_POST['comments'];        
+
+       
+
+            $sql="INSERT INTO feedback(name,email,comments) VALUES('$name','$email','$comments')";
+
+            if($conn->query($sql))
+
+            {
+                ?>
+                    <script>
+                        alert("Record insert successfully");
+                    </script>
+                <?php
+                ?>
+                    <script>
+                        location.replace("../index.html");
+                    </script>
+                <?php
+            }
+            else
+            {                
+                ?>
+                    <script>
+                        alert("Record insert failed");
+                    </script>
+                <?php
+                ?>
+                    <script>
+                        location.replace("../index.html");
+                    </script>
+                <?php
+            }
+        }
+    
+
+?>
