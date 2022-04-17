02e44eabe42eb49b753f59627ffd2791669d1934
Mahesh <maheshmalusare35@gmail.com>
2022-04-17 12:23:10 +0530

    commit

--- php/student.php
+++ php/student.php
@@ -29,7 +29,6 @@ if (!isset($_SESSION['firstname']) || $_SESSION['role']!= "Student") {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <!-- Favicon -->
     <link rel="icon" href="logo/icons8-bank-cards-48.png" />
